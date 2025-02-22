3fd242b648e93c9bcac252f2d760cd72e954b156
Shuvojit Samanta <142198651+shuvojitss@users.noreply.github.com>
2025-02-22 11:40:11 +0530

    Update DAY3.sql

--- DAY3.sql
+++ DAY3.sql
@@ -62,7 +62,7 @@ INSERT INTO Client_master_SS66 (Client_no, Name, City, Pincode, State, Bal_due) 
 ('C00001', 'Ivan Bayross', 'Mumbai', 400054, 'Maharashtra', 15000),
 ('C00002', 'Mamta Mazumdar', 'Madras', 780001, 'Tamil Nadu', 0),
 ('C00003', 'Chhaya Bankar', 'Mumbai', 400057, 'Maharashtra', 5000),
-('C00004', 'Ashwini Joshi', 'Bangalore', 560001, 'Karnataka', 2000),
+('C00004', 'Ashwini Joshi', 'Bangalore', 560001, 'Karnataka', 0),
 ('C00005', 'Hansel Colaco', 'Mumbai', 400060, 'Maharashtra', 2000),
 ('C00006', 'Deepak Sharma', 'Mangalore', 560050, 'Karnataka', 0);
 
