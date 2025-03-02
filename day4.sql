6bbab63c0706faad50b91c1a471972ff63e8d3c4
Shuvojit Samanta <142198651+shuvojitss@users.noreply.github.com>
2025-03-02 13:24:26 +0530

    Update day4.sql

--- day4.sql
+++ day4.sql
@@ -74,3 +74,145 @@ SQL> SELECT * FROM DEPT_SS66;
         30 Sales                Chicago
         40 Operations           Boston
 
+f-p
+SELECT ENAME
+FROM EMP_SS66
+WHERE ENAME LIKE '_i%';
+/*
+ENAME
+-----
+Allen
+Jones
+Blake
+Clark
+Scott
+King 
+Ford 
+*/
+SELECT ENAME
+FROM EMP_SS66
+WHERE JOB != 'Manager';
+/*
+ENAME 
+------
+Smith 
+Allen 
+Ward  
+Martin
+Scott 
+King  
+Turner
+Adams 
+James 
+Ford  
+Miller
+*/
+SELECT 
+    MAX(SAL) AS "Maximum",
+    MIN(SAL) AS "Minimum",
+    SUM(SAL) AS "Total",
+    AVG(SAL) AS "Average"
+FROM EMP_SS66;
+/*
+Maximum  Minimum  Total  Average         
+-------  -------  -----  ----------------
+5000     800      29025  2073.21428571429
+*/
+SELECT 
+    JOB,
+    MAX(SAL) AS "Maximum",
+    MIN(SAL) AS "Minimum",
+    SUM(SAL) AS "Total",
+    AVG(SAL) AS "Average"
+FROM EMP_SS66
+GROUP BY JOB;
+/*
+JOB        Maximum  Minimum  Total  Average         
+---------  -------  -------  -----  ----------------
+Analyst    3000     3000     6000   3000.0          
+Cleark     800      800      800    800.0           
+Clerk      1300     950      3350   1116.66666666667
+Manager    2975     2450     8275   2758.33333333333
+President  5000     5000     5000   5000.0          
+Salesman   1600     1250     5600   1400.0      
+*/
+SELECT COUNT(*) AS "Number of managers"
+FROM EMP_SS66
+WHERE JOB = 'Manager';
+/*
+Number of managers
+------------------
+3   
+*/
+SELECT ENAME
+FROM EMP_SS66
+WHERE HIREDATE > TO_DATE('01-JAN-1980', 'DD-MON-YYYY');
+/*
+ENAME 
+------
+Allen 
+Ward  
+Jones 
+Martin
+Clark 
+Scott 
+Turner
+James 
+Ford  
+Miller
+*/
+SELECT ENAME
+FROM EMP_SS66
+WHERE JOB = 'Salesman'
+AND SAL = (SELECT MAX(SAL) FROM EMP_SS66 WHERE JOB = 'Salesman');
+/*
+ENAME
+-----
+Allen
+*/
+SELECT ENAME
+FROM EMP_SS66
+WHERE JOB = 'Clerk'
+AND SAL = (SELECT MIN(SAL) FROM EMP_SS66 WHERE JOB = 'Clerk');
+/*
+ENAME
+-----
+James
+*/
+SELECT DNAME
+FROM DEPT_SS66
+WHERE DEPTNO = (SELECT DEPTNO FROM EMP_SS66 WHERE ENAME = 'Ford');
+/*
+DNAME   
+--------
+Research
+*/
+SELECT DNAME
+FROM DEPT_SS66
+WHERE DEPTNO = (SELECT DEPTNO 
+                FROM EMP_SS66
+                WHERE SAL = (SELECT MAX(SAL) FROM EMP_SS66));
+/*
+DNAME     
+----------
+Accounting
+*/
+SELECT ENAME
+FROM EMP_SS66
+WHERE SAL > (SELECT SAL FROM EMP_SS66 WHERE ENAME = 'Turner');
+/*
+ENAME
+-----
+Allen
+Jones
+Blake
+Clark
+Scott
+King 
+Ford 
+*/
+
+
+
+
+
