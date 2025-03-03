6e898471ae32a1e2edede72ee8dff2e309c37d14
Shuvojit Samanta <142198651+shuvojitss@users.noreply.github.com>
2025-03-03 11:46:07 +0530

    Update day5.sql

--- day5.sql
+++ day5.sql
@@ -33,4 +33,44 @@ INSERT INTO RESERVES VALUES (10001, 20002, 'Tuesday');
 INSERT INTO RESERVES VALUES (10002, 20002, 'Wednesday');
 INSERT INTO RESERVES VALUES (10003, 20003, 'Friday');
 
+SELECT B.color
+FROM SAILORS_SS66 S
+INNER JOIN RESERVES_SS66 R ON S.s_id = R.s_id
+INNER JOIN BOATS_SS66 B ON R.b_id = B.b_id
+WHERE S.s_name = 'Tarun';
+/*
+COLOR
+--------------------
+Red
+Green
+*/
+
+SELECT S.s_id, S.s_name
+FROM SAILORS_SS66 S
+INNER JOIN RESERVES_SS66 R ON S.s_id = R.s_id
+WHERE R.day = 'Monday';
+/*
+S_ID S_NAME
+---------- --------------------------------------------------
+     10001 Tarun
+*/
+
+SELECT b_id, b_name
+FROM BOATS_SS66
+WHERE color IN ('Red', 'Green');
+/*B_ID B_NAME
+---------- --------------------------------------------------
+     20001 Titanic
+     20002 Voyager
+*/
+DELETE FROM RESERVES_SS66
+WHERE s_id IN (SELECT s_id FROM SAILORS_SS66 WHERE age > 60);
+
+DELETE FROM SAILORS_SS66
+WHERE age > 60;
+/*---------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+
 
